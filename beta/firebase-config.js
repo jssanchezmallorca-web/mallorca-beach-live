0ac98fc4d962d49bc204ab8436b773d09eebad98
jssanchezmallorca-web <jssanchezmallorca@gmail.com>
2026-08-25 21:24:16 +0200

    Add separate family tracker identity configuration

--- beta/firebase-config.js
+++ beta/firebase-config.js
@@ -1,1 +1,1 @@
-window.BEACH_CAM_FIREBASE={enabled:false,masterEmail:'',firebaseConfig:{apiKey:'',authDomain:'',databaseURL:'',projectId:'',appId:''}};
+window.BEACH_CAM_FIREBASE={enabled:false,masterEmail:'',trackerEmail:'',firebaseConfig:{apiKey:'',authDomain:'',databaseURL:'',projectId:'',appId:''}};
