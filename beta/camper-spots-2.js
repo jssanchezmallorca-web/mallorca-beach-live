5e9e600e315d593b6ece7d0946a603f4d68d8676
jssanchezmallorca-web <jssanchezmallorca@gmail.com>
2026-08-26 03:03:07 +0200

    Add second batch of extended family camper spots

---
+++ beta/camper-spots-2.js
@@ -0,0 +1,21 @@
+window.CAMPER_SPOT_LINES=(window.CAMPER_SPOT_LINES||[]).concat(`mondrago-amarador|Mondragó · s’Amarador|Santanyí|39.3508|3.1894|parking|Pago / revisar|1|1|warn|Zona natural protegida; comprobar acceso y horario.|https://park4night.com/es/place/69793
+calesmallorca|Cales de Mallorca · Carrer Aucanada|Manacor|39.4684|3.2704|parking|Gratis|1|1|warn|Parking amplio; revisar señalización.|https://park4night.com/es/place/643334
+caladomingos|Cala Domingos · parking playa|Manacor|39.4584|3.279|parking|Gratis|1|1|warn|Para vehículos largos, aparcar sin bloquear maniobras.|https://park4night.com/es/place/443180
+portocristo-goleta|Porto Cristo · Carrer de la Goleta|Manacor|39.5467|3.3423|street|Gratis|1|1|warn|Zona urbana; máxima discreción.|https://park4night.com/es/place/551753
+portocristo-sol|Porto Cristo · Sol Ponent|Manacor|39.5383|3.339|street|Gratis|1|1|warn|Pequeño final de calle; no bloquear accesos.|https://park4night.com/es/place/567221
+sacoma-beach|Sa Coma · Avinguda de les Savines|Sant Llorenç|39.5722|3.3748|parking|Gratis|1|1|warn|Plazas pequeñas y muy ocupadas en verano.|https://park4night.com/es/place/568116
+sacoma-main|Sa Coma · Ses Palmeres|Sant Llorenç|39.5763|3.3775|parking|Gratis|1|1|warn|Puede ser ruidoso y llenarse durante el día.|https://park4night.com/es/place/240432
+calamillor|Cala Millor · Camí de Son Moro|Sant Llorenç|39.5882|3.3824|parking|Gratis|1|1|warn|Explanada de tierra; comprobar estado tras lluvias.|https://park4night.com/es/place/453877
+calabona|Cala Bona · parking de pinos|Son Servera|39.6061|3.3832|parking|Gratis|1|1|warn|Zona residencial; respeto a vecinos.|https://park4night.com/es/place/386804
+fontsa|Font de sa Cala · parking playa|Capdepera|39.6824|3.4512|parking|Gratis|1|1|warn|Muy concurrido en temporada alta.|https://park4night.com/es/place/419755
+can-pacienci|Pollença · Ca’n Pacienci|Pollença|39.8884|3.0293|area|Pago|1|0|ok|Área privada con servicios; consultar precio actual.|https://www.park4night.com/es/place/423205
+calvia-palma-andratx|Calvià · Carretera Palma-Andratx|Calvià|39.5208|2.5738|parking|Gratis|1|1|warn|Parking público; revisar limitaciones de tamaño y señales.|https://park4night.com/en/place/148621
+palma-gaston|Palma · Carrer Gaston Vuillier|Palma|39.5736|2.6829|parking|Gratis|1|0|warn|Amplio y usado por campers; entorno urbano.|https://park4night.com/es/place/525678
+palmanova-mino|Palmanova · Carrer Miño|Calvià|39.5121|2.5308|parking|Gratis|1|1|warn|Parking junto a vía principal; puede haber ruido.|https://park4night.com/es/place/497968
+bendinat-centre|Bendinat Centre · parking municipal|Calvià|39.5353|2.5872|parking|Gratis|1|1|warn|Municipal y tranquilo según referencias; revisar señalización.|https://park4night.com/es/place/357213
+passatge-mar|Calvià · Passatge a la Mar|Calvià|39.5334|2.5726|parking|Gratis|1|1|warn|Altura indicada 4 m; cerca de playa.|https://park4night.com/en/place/694550
+playa-palma-quiet|Playa de Palma · calle tranquila|Palma|39.5134|2.7552|street|Gratis|1|1|warn|Estacionamiento en calle; respetar vecinos y señales.|https://park4night.com/es/place/614715
+portsoller-reis|Port de Sóller · Plaça Reis de Mallorca|Sóller|39.7991|2.6986|parking|Pago|1|1|warn|Pago diurno; cerca del paseo y tranvía.|https://park4night.com/es/place/297513
+valldemossa-george|Valldemossa · George Sand|Valldemossa|39.7051|2.5806|parking|Gratis|1|1|warn|Pequeño, tranquilo y con vistas; pocas plazas.|https://park4night.com/es/place/579008
+soller-ma2124|Sóller · Ma-2124 mirador|Sóller|39.7951|2.714|parking|Gratis|1|0|warn|Muy pequeño; solo un par de plazas.|https://park4night.com/es/place/531555
+portsoller-pol1|Port de Sóller · Calle Polígono 1|Sóller|39.7892|2.691|parking|Pago|1|1|warn|Cerca de playa; regulación diurna.|https://park4night.com/es/place/282507`.trim().split('\n'));
